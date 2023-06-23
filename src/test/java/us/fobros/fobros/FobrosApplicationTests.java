82db7f4c1dc57f17a30453ce44400197f212598c
Sardor Ubaydullayev <sardorubaydullayev20@gmail.com>
2023-06-23 13:54:01 +0500

    add Java

---
+++ src/test/java/us/fobros/fobros/FobrosApplicationTests.java
@@ -0,0 +1,13 @@
+package us.fobros.fobros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FobrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
