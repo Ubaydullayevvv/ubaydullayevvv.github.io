01660804263867d085e04024986cabccbc274e07
Sardor Ubaydullayev <sardorubaydullayev20@gmail.com>
2023-06-23 13:53:53 +0500

    add Java

---
+++ src/main/java/us/fobros/fobros/FobrosApplication.java
@@ -0,0 +1,13 @@
+package us.fobros.fobros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FobrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FobrosApplication.class, args);
+    }
+
+}
